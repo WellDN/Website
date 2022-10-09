48577f3ba4f0f959fbc93205802a030ecb099c8c
w <w@pop-os.localdomain>
2022-10-09 16:26:50 -0300

    components

--- src/animation.tsx
+++ src/animation.tsx
@@ -1,10 +1,20 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from "@react-three/fiber";
 
-export function Animation() {
-    <Canvas>
-        
-    </Canvas>
 
-    
-    return
+export const Animation = (props: JSX.IntrinsicElements) => {
+  <Canvas>
+    <pointLight position={[10, 10, 10]} />
+  <mesh>
+    <sphereGeometry />
+    <meshStandardMaterial color="hotpink" />
+  </mesh>
+  </Canvas>
 }
+
+
+
+/*function iAnimation() {
+
+
+  return
+}*/

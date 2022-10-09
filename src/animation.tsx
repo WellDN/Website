c9a9f1df9a280610d9ad9ef6eff6135ac38fb75f
w <w@pop-os.localdomain>
2022-10-09 16:26:50 -0300

    components

--- src/animation.tsx
+++ src/animation.tsx
@@ -1,10 +1,24 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from "@react-three/fiber";
+
 
 export function Animation() {
-    <Canvas>
-        
-    </Canvas>
-
-    
-    return
+  return(
+    <div className="h-screen w-screen -z-50 opacity-50 pointer-events-none absolute">
+  <Canvas>
+    <pointLight position={[10, 10, 10]} />
+  <mesh>
+    <sphereGeometry />
+    <meshStandardMaterial color="red" />
+  </mesh>
+  </Canvas>
+  </div>
+  )
 }
+
+
+
+/*function iAnimation() {
+
+
+  return
+}*/
